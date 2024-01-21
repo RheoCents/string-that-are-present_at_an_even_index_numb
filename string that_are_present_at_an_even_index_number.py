eaca1a0586a8a565474f5bedd90b15cd7871f580
unknown <centinaraphael0157@gmail.com>
2024-01-21 22:07:01 +0800

    working code

--- string that_are_present_at_an_even_index_number.py
+++ string that_are_present_at_an_even_index_number.py
@@ -1,5 +1,6 @@
 # Write a program to accept a string from the user and display characters that are present at an even index number.
 user_input_name = input('What is your name ')
-input_user_number_of_letters = input('how many letters is in your name ') 
-for i in range (0, int(input_user_number_of_letters), 2):
+user_input_name_number = len(user_input_name)
+#input_user_number_of_letters = input('how many letters is in your name ') 
+for i in range (0, user_input_name_number, 2):
     print(user_input_name[i])
